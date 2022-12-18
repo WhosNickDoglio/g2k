7ca91612bcb42477d6211b832272714c7ef3037c
nicholasdoglio <NicholasDoglio@Gmail.com>
2022-12-18 08:34:17 -0500

    Update detekt config

--- src/main/kotlin/dev/whosnickdoglio/g2k/ConvertGroovyToKotlinAction.kt
+++ src/main/kotlin/dev/whosnickdoglio/g2k/ConvertGroovyToKotlinAction.kt
@@ -32,7 +32,6 @@ import com.intellij.openapi.actionSystem.AnActionEvent
  */
 internal class ConvertGroovyToKotlinAction : AnAction() {
 
-
     override fun actionPerformed(event: AnActionEvent) {
         TODO("Not yet implemented")
     }
